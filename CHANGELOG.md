10a80f7e47fdbcf01879711b66c0b64453202a9d
stiofan <stiofansisland@gmail.com>
2026-04-09 11:59:52 +0100

    3.0.1-bet

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+All notable changes to the AyeCode Template Manager will be documented in this file.
+
+## [3.0.1-beta] - 2026-04-09
+- composer package type wrong - FIXED
+
+## [3.0.0-beta] - 2026-04-07
+- Initial beta release
--- wp-ayecode-template-manager.php
+++ wp-ayecode-template-manager.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP AyeCode Template Manager
  * Plugin URI: https://ayecode.io/
  * Description: Centralized template management hub for the AyeCode ecosystem.
- * Version: 3.0.0-beta
+ * Version: 3.0.1-beta
  * Author: AyeCode Ltd
  * Author URI: https://ayecode.io/
  * License: GPL-2.0+
@@ -23,7 +23,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // Define plugin constants.
 if ( ! defined( 'AYECODE_TEMPLATE_MANAGER_VERSION' ) ) {
-	define( 'AYECODE_TEMPLATE_MANAGER_VERSION', '3.0.0-beta' );
+	define( 'AYECODE_TEMPLATE_MANAGER_VERSION', '3.0.1-beta' );
 }
 if ( ! defined( 'AYECODE_TEMPLATE_MANAGER_PLUGIN_FILE' ) ) {
 	define( 'AYECODE_TEMPLATE_MANAGER_PLUGIN_FILE', __FILE__ );
